dd142ae3bd0906fef4ded6cbef63b5a5115d2ed6
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-08-11 14:19:05 +0530

    Update main.py

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -8,3 +8,15 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 shift = shift % 26
+def caesarCipher(startText, shiftAmount, cipherDirection):
+    endText = ""
+    if cipherDirection == "decode":
+        shiftAmount *= -1
+    for letter in startText:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            endText += alphabet[index+shiftAmount]
+        else:
+            endText += letter
+
+    print(f"Here the {cipherDirection} result: {endText}") 
